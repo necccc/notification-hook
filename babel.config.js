212d4cf43a3422909cb445e5674d0f632a93291a
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2019-11-06 16:14:19 +0100

    set up build & test

---
+++ babel.config.js
@@ -0,0 +1,58 @@
+let defaultPresets;
+
+if (process.env.BABEL_ENV === 'es') {
+  defaultPresets = [];
+} else {
+  defaultPresets = [
+    [
+      '@babel/preset-env',
+      {
+        modules: ['esm', 'production-umd'].includes(process.env.BABEL_ENV) ? false : 'commonjs',
+      },
+    ],
+  ];
+}
+
+const productionPlugins = [
+  'babel-plugin-transform-react-constant-elements',
+  'babel-plugin-transform-dev-warning',
+  [
+    'babel-plugin-transform-react-remove-prop-types',
+    {
+      mode: 'unsafe-wrap',
+    },
+  ],
+];
+
+module.exports = {
+  presets: defaultPresets.concat(['@babel/preset-react']),
+  plugins: [
+    'babel-plugin-optimize-clsx',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+    '@babel/plugin-transform-runtime',
+    // for IE 11 support
+    '@babel/plugin-transform-object-assign',
+  ],
+  ignore: [/@babel[\\|/]runtime/], // Fix a Windows issue.
+  env: {
+    cjs: {
+      plugins: productionPlugins,
+    },
+    esm: {
+      plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+    },
+    es: {
+      plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+    },
+    production: {
+      plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+    },
+    'production-umd': {
+      plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+    },
+    test: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }
+  },
+};
---
+++ src/hook.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { renderHook, act } from '@testing-library/react-hooks'
+import Context from './context'
+import useNotification from './hook'
+
+describe('hook', () => {
+  it('should work with success', () => {
+
+    const wrapper = ({ children }) => <Context.Provider value={ [] }>{children}</Context.Provider>
+    const { result } = renderHook(() => useNotification(), { wrapper })
+
+    act(() => {
+      result.current.showSuccess('Yay!')
+    })
+
+    expect(result.current.context).toHaveLength(1)
+    expect(result.current.context[0]).toHaveProperty('type', 'success');
+    expect(result.current.context[0]).toHaveProperty('message', 'Yay!');
+    expect(result.current.context[0]).toHaveProperty('duration', 5000);
+    expect(result.current.context[0]).toHaveProperty('open', true);
+    expect(result.current.context[0]).toHaveProperty('id');
+  })
+
+  it('should work with error', () => {
+
+    const wrapper = ({ children }) => <Context.Provider value={ [] }>{children}</Context.Provider>
+    const { result } = renderHook(() => useNotification(), { wrapper })
+
+    act(() => {
+      result.current.showError('Oh noes!', 2000)
+    })
+
+    expect(result.current.context).toHaveLength(1)
+    expect(result.current.context[0]).toHaveProperty('type', 'error');
+    expect(result.current.context[0]).toHaveProperty('message', 'Oh noes!');
+    expect(result.current.context[0]).toHaveProperty('duration', 2000);
+
+  })
+})
